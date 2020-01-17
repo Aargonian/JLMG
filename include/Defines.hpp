18fa141b7e8c895e93c2e5450d9e8543b3b7b1e7
Aaron Helton <Aargonian@gmail.com>
2020-01-16 22:31:24 -0500

    Added a Defines File

---
+++ include/Defines.hpp
@@ -0,0 +1,19 @@
+//
+// Created by Aaron Helton on 1/16/2020.
+//
+
+#ifndef JLMG_DEFINES_HPP
+#define JLMG_DEFINES_HPP
+
+#include <cstdint>
+
+typedef uint_least8_t uint8;
+typedef uint_least16_t uint16;
+typedef uint_least32_t uint32;
+typedef uint_least64_t uint64;
+typedef int_least8_t int8;
+typedef int_least16_t int16;
+typedef int_least32_t int32;
+typedef int_least64_t int64;
+
+#endif //JLMG_DEFINES_HPP
